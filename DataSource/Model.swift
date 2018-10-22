502cfa2d0ea69449bb4431e974dcf819d8e7c46e
Marcin Karski <karski80@gmail.com>
2018-10-22 16:45:06 +0100

    Working model

--- DataSource/Model.swift
+++ DataSource/Model.swift
@@ -1,6 +1,15 @@
 import Foundation
 
 struct Model {
-    var name: String
-    var image: String
+    let name: String
+    let image: String
+}
+
+func fetchData() -> [Model] {
+    let img1 = Model(name: "Puerto del Carmen", image: "img1")
+    let img2 = Model(name: "Roque Nublo", image: "img2")
+    let img3 = Model(name: "La Santa", image: "img3")
+    let img4 = Model(name: "Playa de Las Teresitas", image: "img4")
+    let img5 = Model(name: "Playa de La Tejita", image: "img5")
+    return [img1, img2, img3, img4, img5]
 }
--- DataSource/ViewController.swift
+++ DataSource/ViewController.swift
@@ -1,17 +1,16 @@
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    private let model = fetchData()
     
     lazy var tableView: UITableView = {
-        let tableView = UITableView(frame: view.bounds, style: .plain)
-        tableView.estimatedRowHeight = 100
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        let tableView = UITableView(frame: view.bounds, style: .grouped)
+        tableView.register(TableCell.self, forCellReuseIdentifier: "Cell")
         tableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-//        tableView.separatorStyle = .none
         tableView.separatorInset = .zero
-//        tableView.delegate = self
-        //        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.dataSource = self
         return tableView
     }()
 
@@ -22,5 +21,36 @@ class ViewController: UIViewController {
     
     private func setup() {
         view.addSubview(tableView)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return model.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = model[indexPath.row].name
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+final class TableCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.accessoryType = .disclosureIndicator
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
